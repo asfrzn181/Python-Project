9a7c735543f5fdf45e1fb4b75b2047c0abadacb5
Muhamad Asfarezhan <61591815+asfrzn181@users.noreply.github.com>
2022-09-12 12:59:36 +0700

    Add files via upload

---
+++ TelegramChat/config.py
@@ -0,0 +1,6 @@
+import os
+
+TOKEN = os.environ.get("TOKEN", "5707918567:AAFH5BHPTisssBdRBcVNoLDwomhTfr03GM0") # Dapatkan di @botfather
+OWNER = os.environ.get("OWNER", "Tamahg") # Isi dengan username kalian tanpa tanda @
+GROUP = os.environ.get("GROUP", "") # Isi dengan username group kalian tanpa tanda @ kalau gak punya gak usah isi
+CHANNEL = os.environ.get("CHANNEL", "") # Isi dengan username channel kalian tanpa tanda @ kalau gak punya gak usah isi
